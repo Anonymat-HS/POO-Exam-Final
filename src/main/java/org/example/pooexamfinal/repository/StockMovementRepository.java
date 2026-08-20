0b95e4425b058dceb1e5d01e437eead303aeaa46
Herimihaja Steevy RASOAMIARAMANANA <steevyherimihaja@gmail.com>
2026-08-20 15:11:30 +0300

    fix: cast movement type parameter in findByMovementType query

--- src/main/java/org/example/pooexamfinal/repository/StockMovementRepository.java
+++ src/main/java/org/example/pooexamfinal/repository/StockMovementRepository.java
@@ -2,7 +2,9 @@ package org.example.pooexamfinal.repository;
 
 import org.example.pooexamfinal.model.MovementType;
 import org.example.pooexamfinal.model.StockMovement;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,10 @@ import java.util.List;
 public interface StockMovementRepository extends ListCrudRepository<StockMovement, String> {
     List<StockMovement> findByProductId(String productId);
 
-    List<StockMovement> findByMovementType(MovementType movementType);
+    @Query("""
+            SELECT id, created_at, movement_type, quantity, product_id
+            FROM stock_movements
+            WHERE movement_type = CAST(:movementType AS movement_type)
+            """)
+    List<StockMovement> findByMovementType(@Param("movementType") MovementType movementType);
 }
