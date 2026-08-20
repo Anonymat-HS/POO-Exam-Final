53e5a6d2d902bc120c5737e8e705cdfa2939a3ad
Herimihaja Steevy RASOAMIARAMANANA <steevyherimihaja@gmail.com>
2026-08-20 11:56:54 +0300

    feat: initialize project structure and setup

---
+++ src/main/java/org/example/pooexamfinal/PooExamFinalApplication.java
@@ -0,0 +1,13 @@
+package org.example.pooexamfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooExamFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PooExamFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pooexamfinal/PooExamFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pooexamfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooExamFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
